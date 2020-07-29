ef29a19a8e71e761ff5cf93439191dc5647da834
EzhilaLylu <68938341+EzhilaLylu@users.noreply.github.com>
2020-07-29 10:24:52 +0530

    InitialSetup

---
+++ bank-app-ws/src/main/java/com/cognizant/mc/bankappws/BankAppWsApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.mc.bankappws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAppWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAppWsApplication.class, args);
+	}
+
+}
---
+++ bank-app-ws/src/test/java/com/cognizant/mc/bankappws/BankAppWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.mc.bankappws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAppWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
